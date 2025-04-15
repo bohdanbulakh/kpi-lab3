628cd2e08aa92f114ce2403675384d430cb946aa
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-04-15 21:38:59 +0300

    implemented visualiser

--- ui/window.go
+++ ui/window.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"golang.org/x/image/colornames"
 	"image"
 	"image/color"
 	"log"
@@ -19,124 +20,159 @@ import (
 type Visualizer struct {
 	Title         string
 	Debug         bool
-	OnScreenReady func(s screen.Screen)
+	OnScreenReady func(screen screen.Screen)
 
-	w    screen.Window
-	tx   chan screen.Texture
-	done chan struct{}
+	window  screen.Window
+	texture chan screen.Texture
+	done    chan struct{}
 
-	sz  size.Event
-	pos image.Rectangle
+	size   size.Event
+	pos    image.Rectangle
+	center image.Point
 }
 
-func (pw *Visualizer) Main() {
-	pw.tx = make(chan screen.Texture)
-	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
-	driver.Main(pw.run)
+func (visualizer *Visualizer) Main() {
+	visualizer.texture = make(chan screen.Texture)
+	visualizer.done = make(chan struct{})
+	visualizer.center.X = 400
+	visualizer.center.Y = 400
+	driver.Main(visualizer.run)
 }
 
-func (pw *Visualizer) Update(t screen.Texture) {
-	pw.tx <- t
+func (visualizer *Visualizer) Update(t screen.Texture) {
+	visualizer.texture <- t
 }
 
-func (pw *Visualizer) run(s screen.Screen) {
-	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+func (visualizer *Visualizer) run(s screen.Screen) {
+	if visualizer.OnScreenReady != nil {
+		visualizer.OnScreenReady(s)
+	}
+
+	window, err := s.NewWindow(&screen.NewWindowOptions{
+		Title:  visualizer.Title,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
 	}
 	defer func() {
-		w.Release()
-		close(pw.done)
+		window.Release()
+		close(visualizer.done)
 	}()
 
-	if pw.OnScreenReady != nil {
-		pw.OnScreenReady(s)
-	}
-
-	pw.w = w
+	visualizer.window = window
 
 	events := make(chan any)
 	go func() {
 		for {
-			e := w.NextEvent()
-			if pw.Debug {
-				log.Printf("new event: %v", e)
+			event := window.NextEvent()
+			if visualizer.Debug {
+				log.Printf("new event: %v", event)
 			}
-			if detectTerminate(e) {
+			if detectTerminate(event) {
 				close(events)
 				break
 			}
-			events <- e
+			events <- event
 		}
 	}()
 
-	var t screen.Texture
+	var texture screen.Texture
 
 	for {
 		select {
-		case e, ok := <-events:
+		case event, ok := <-events:
 			if !ok {
 				return
 			}
-			pw.handleEvent(e, t)
+			visualizer.handleEvent(event, texture)
 
-		case t = <-pw.tx:
-			w.Send(paint.Event{})
+		case texture = <-visualizer.texture:
+			window.Send(paint.Event{})
 		}
 	}
 }
 
-func detectTerminate(e any) bool {
-	switch e := e.(type) {
+func detectTerminate(event any) bool {
+	switch event := event.(type) {
 	case lifecycle.Event:
-		if e.To == lifecycle.StageDead {
+		if event.To == lifecycle.StageDead {
 			return true // Window destroy initiated.
 		}
 	case key.Event:
-		if e.Code == key.CodeEscape {
+		if event.Code == key.CodeEscape {
 			return true // Esc pressed.
 		}
 	}
 	return false
 }
 
-func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
-	switch e := e.(type) {
+func (visualizer *Visualizer) handleEvent(event any, texture screen.Texture) {
+	switch event := event.(type) {
 
 	case size.Event: // Оновлення даних про розмір вікна.
-		pw.sz = e
+		visualizer.size = event
+		visualizer.center = image.Pt(
+			visualizer.size.WidthPx/2,
+			visualizer.size.HeightPx/2,
+		)
 
 	case error:
-		log.Printf("ERROR: %s", e)
+		log.Printf("ERROR: %s", event)
 
 	case mouse.Event:
-		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
+		if texture == nil {
+			if event.Button == mouse.ButtonLeft &&
+				event.Direction == mouse.DirPress {
+				visualizer.center.Y, visualizer.center.X = int(event.Y), int(event.X)
+				visualizer.window.Send(paint.Event{})
+			}
 		}
 
 	case paint.Event:
 		// Малювання контенту вікна.
-		if t == nil {
-			pw.drawDefaultUI()
+		if texture == nil {
+			visualizer.drawDefaultUI()
 		} else {
 			// Використання текстури отриманої через виклик Update.
-			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
+			visualizer.window.Scale(
+				visualizer.size.Bounds(),
+				texture,
+				texture.Bounds(),
+				draw.Src,
+				nil,
+			)
 		}
-		pw.w.Publish()
+		visualizer.window.Publish()
 	}
 }
 
-func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+func DrawFigure(uploader screen.Uploader, point image.Point) {
+	uploader.Fill(
+		image.Rect(point.X+200, point.Y, point.X-200, point.Y-175),
+		colornames.Blue,
+		draw.Src,
+	)
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	uploader.Fill(
+		image.Rect(point.X-75, point.Y, point.X+75, point.Y+175),
+		colornames.Blue,
+		draw.Src,
+	)
+}
+
+func (visualizer *Visualizer) drawDefaultUI() {
+	visualizer.window.Fill(
+		visualizer.size.Bounds(),
+		colornames.Lime,
+		draw.Src,
+	) // Фон.
+
+	DrawFigure(visualizer.window, visualizer.center)
 
 	// Малювання білої рамки.
-	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
-		pw.w.Fill(br, color.White, draw.Src)
+	for _, border := range imageutil.Border(visualizer.size.Bounds(), 10) {
+		visualizer.window.Fill(border, color.White, draw.Src)
 	}
 }
